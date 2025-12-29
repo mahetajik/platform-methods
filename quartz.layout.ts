86b8be1ffb49b009312bf9eea0368478112046f3
mahetajik <105546819+mahetajik@users.noreply.github.com>
2025-12-29 11:12:24 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,8 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    // UPDATED: Title removed
+    Component.Explorer({ title: "" }),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +63,8 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    // UPDATED: Title removed
+    Component.Explorer({ title: "" }),
   ],
   right: [],
 }
