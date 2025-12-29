8847d9dd018777d7785c89a238aa9468b4d986ff
mahetajik <105546819+mahetajik@users.noreply.github.com>
2025-12-29 09:38:20 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      Website: "https://kausharmahetaji.com",
+      Kaushar Mahetaji: "https://kausharmahetaji.com",
       "Discord Community": "",
     },
   }),
