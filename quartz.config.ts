93ef8005b5be06a5743f98da1e1b1a685553a63a
mahetajik <105546819+mahetajik@users.noreply.github.com>
2025-12-29 08:50:41 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "www.kausharmahetaji.com/platform-methods",
+    baseUrl: "kausharmahetaji.github.io/platform-methods",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
