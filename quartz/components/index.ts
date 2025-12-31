3b07ba1653ce6ae6195215fa87b096969b705a7e
mahetajik <105546819+mahetajik@users.noreply.github.com>
2025-12-31 09:32:45 -0500

    Update index.ts

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -23,6 +23,7 @@ import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
+import PageList from "./PageList" // <--- ADDED THIS IMPORT
 
 export {
   ArticleTitle,
@@ -50,4 +51,5 @@ export {
   Comments,
   Flex,
   ConditionalRender,
+  PageList, // <--- ADDED THIS EXPORT
 }
