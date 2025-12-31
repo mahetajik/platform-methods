92d8206b8e9aa549dd6af83ad8c75132002fed2c
mahetajik <105546819+mahetajik@users.noreply.github.com>
2025-12-31 14:09:55 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,7 +45,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      "GitHub": "https://github.com/mahetajik/platform-methods",
+      //"GitHub": "https://github.com/mahetajik/platform-methods",
     },
   }),
 }
